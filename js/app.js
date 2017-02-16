acfe88008e63d51b9606263ae1e1c21fad0848ff
Justyna <justynazarudzka@gmail.com>
2017-02-16 09:20:51 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,20 +1,6 @@
-var main = function() {
-    $('.icon-menu').click(function() {
-      $('.menu').animate( {
-        left: '0px'
-      }, 200);
-      
-      $('body').animate ( {
-          left: '285px'
-      }, 200);
+var main = function () {
+    $('.icon-menu').click(function () {
+      $('.menu').toggle();
     });
-    $('.icon-close').click(function() {
-      $('.menu').animate( {
-        left: '-285px'
-      }, 200);
-      $('body').animate ( {
-          left: '0px'
-      }, 200);
-    });
-}
+  };
 $(document).ready(main);
